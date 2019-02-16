3bb938c092436406ded146cfb063a176907465f7
David Toledo <dtoledoz0207@gmail.com>
2019-02-16 15:06:29 -0600

    functionalitySearchNavBar-Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'buscar', component: BuscarComponent },
+    { path: 'buscar/:texto', component: BuscarComponent },
     { path: 'pelicula', component: PeliculaComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PeliculasService} from '../../services/peliculas.service';
-
+import {ActivatedRoute} from '@angular/router';
+ 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
@@ -10,7 +11,17 @@ export class BuscarComponent implements OnInit {
 
   buscar:string = "";
 
-  constructor(public _ps:PeliculasService) { }
+  constructor(public _ps:PeliculasService, private activatedRoute:ActivatedRoute) { 
+
+    this.activatedRoute.params.subscribe(parametros => {
+      console.log(parametros['texto']);
+      if(parametros['texto']){
+        this.buscar = parametros['texto'];
+        this.buscarPelicula();
+      }
+    });
+
+  }
 
   ngOnInit() {
   }
